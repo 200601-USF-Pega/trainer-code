878e0d73a9d98f1ea517213957851ddf088ca809
mariellenolasco <marielle.nolasco@revature.com>
2020-06-17 13:10:25 -0700

    servlet demo

---
+++ Week3Servlets,UI/ServletDemo/src/main/java/com/revature/servletdemo/servlets/ServletUno.java
@@ -0,0 +1,29 @@
+package com.revature.servletdemo.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletUno extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("In the get method of the Servlet Uno class");
+        resp.setContentType("text/html");
+        resp.getWriter().write(
+                "<html>" +
+                "<body>" +
+                "<p>" +
+                "Welcome to the get place" +
+                "</p>" +
+                "</body>" +
+                "</html>"
+        );
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("In the delete method of servlet uno");
+    }
+}
